19f212597cc71d0df13110af3eb67a6d3ee0781c
SantiagoOrtiz <santiagoorla@unisabana.edu.co>
2023-04-17 19:27:15 -0400

    Print message when T below Antoine Equation are provided

--- src/solving_eos.py
+++ src/solving_eos.py
@@ -88,14 +88,16 @@ class EOS:
     
         def evalT(TP, antoineq=self.antoineq):
             T, P = TP
-            if (T < self.Tc) and (P < self.Pc):
+            if (T > antoineq['T1']) and (T < self.Tc) and (P < self.Pc):
                 abcrow = antoineq[(antoineq['T1'] < T) & (T < antoineq['T2'])]
                 A = abcrow.iloc[0]['A']
                 B = abcrow.iloc[0]['B']
                 C = abcrow.iloc[0]['C']
                 return np.power(10, A - B/(T + C))
+            elif T < antoineq['T1']:
+                print(f'The provided temperatures are out of bounds in the Antonine Equation. : T<{antoineq["T1"]}')
             else:
-                print(f'Some of the given T and P values lie outside the range of critical conditions: Tc={self.Tc} & Pc={self.Pc}')
+                print(f'Some of the given T and P values lie outside the range (>) of critical conditions: Tc={self.Tc} & Pc={self.Pc}')
                 return self.Pc/1E5
         return np.array([*map(evalT, [*zip(T, P)])])*1E5
     
